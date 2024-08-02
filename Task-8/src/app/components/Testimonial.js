6daba9cc958419d4742db67d0df81f9ca004b322
Pavanviky <108631467+Pavanviky@users.noreply.github.com>
2024-08-02 12:21:05 +0530

    Update Testimonial.js

--- Task-8/src/app/components/Testimonial.js
+++ Task-8/src/app/components/Testimonial.js
@@ -12,7 +12,7 @@ const TestimonialSection = () => {
           <blockquote className={styles.blockquote}>
             <FaQuoteLeft className={styles.quoteIcon} />
             <p className={styles.quoteText}>
-              There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.
+              There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don&apos;t look even slightly believable.
             </p>
             <FaQuoteRight className={styles.quoteIcon} />
           </blockquote>
